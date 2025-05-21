5ecb533847c70b3dacfa491834cf69b903bfcb3d
BlueRana <bluefrog79sl@gmail.com>
2025-05-21 16:34:56 -0500

    Finished final project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -2,8 +2,9 @@ const express = require('express');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
-const axios = require("axios"); //Requiring axois module to make Http request Task 11
+const axios = require("axios"); //Requiring axois module to make Http request Task 11 & 13
 const public_users = express.Router();
+
 
 
 
@@ -32,6 +33,7 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop by promise
 public_users.get('/', function (req, res) {  
     const getAllBooks = new Promise((resolve, reject) => {
+        
         try {  
             const get_books = res.send(JSON.stringify({books}, null, 4));
             resolve(get_books);
@@ -46,7 +48,8 @@ public_users.get('/', function (req, res) {
     );
 });
 
-{/* Task 1
+{/*
+// Task 1
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
     // Send JSON response with formatted books data
@@ -58,11 +61,12 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN with async/await with axios
 // Make sure to install axois in package.json command npm install axios
 public_users.get('/isbn/:isbn', async function (req, res) {
+        const isbn = req.params.isbn;
+
     try{  
         // const response = await axios.get(url)
         const response = await axios.get("http://localhost:5000");
-        const bookISBN = books[isbn];
-        if(bookISBN && response){
+        if(books[isbn]){
             return res.status(200).send(books[isbn]),
             console.log(`Book with ISBN ${isbn}`);
             }else{
@@ -76,7 +80,8 @@ public_users.get('/isbn/:isbn', async function (req, res) {
     
 });
 
-{/* Task 2
+{/*
+ //Task 2
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
       // Retrieve the isbn parameter from the request URL and send the corresponding isbn's details
@@ -90,7 +95,7 @@ const isbn = req.params.isbn;
  //.toLowerCase() is needed so [] of unknown authors will be returned
 public_users.get('/author/:author', async (req, res) => {
     try{
-const author = req.params.author.toLowerCase();
+    const author = req.params.author.toLowerCase();
     const booksByAuthor = Object.keys(books)
       .filter(isbn => books[isbn].author.toLowerCase() === author)
       .map(isbn => ({
@@ -101,7 +106,7 @@ const author = req.params.author.toLowerCase();
       }));
 
     if (booksByAuthor.length > 0) {
-      res.json({ booksByAuthor }, null, 4);
+      res.json({ booksByAuthor });
       console.log(`${author} : Found`);
 
     } else {
@@ -114,10 +119,8 @@ const author = req.params.author.toLowerCase();
   }
 });
 
-
-
-
-{/* Task 3
+{/*
+//Task 3
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
 const author = req.params.author.toLowerCase();
@@ -140,10 +143,14 @@ const author = req.params.author.toLowerCase();
 */}
 
 //Task 13
-// Get all books based on title by async
-public_users.get('/title/:title', async (req, res) => {
-    try{
-const title = req.params.title;
+// Get all books based on title by async await
+public_users.get('/title/:title', async function (req, res) {
+   try {
+    const title = req.params.title;
+    const response = await axios.get('http://localhost:5000');
+    const {books} = response.data; //data assumed to be an object where each key is an ISBN and each value is a book object.
+
+    // Filter and map books by title
     const booksByTitle = Object.keys(books)
       .filter(isbn => books[isbn].title === title)
       .map(isbn => ({
@@ -154,11 +161,11 @@ const title = req.params.title;
       }));
 
     if (booksByTitle.length > 0) {
-      res.json({ booksByTitle }, null, 4);
+      res.json({ booksByTitle });
       console.log(`${title} : Found`);
 
     } else {
-      res.status(404).json({ message: `No books found by the title of ${title}.` });
+      res.status(404).json({ message: `No books found by the titled ${title}.` });
       console.log (`${title} : No Found`)
     }
   } catch (error) {
@@ -169,7 +176,7 @@ const title = req.params.title;
 
 {/*
 // Task 4
-// Get book details based on author
+// Get book details based on title
 public_users.get('/title/:title',function (req, res) {
     const title = req.params.title;
     let booksByTitle = [];
